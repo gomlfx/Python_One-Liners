b21a791c581c6ac1cce0906bd926b28ed777b29e
Mike Smith <mlfxbroker@gmail.com>
2025-07-30 23:26:58 -0700

    Create numpy_array_p44.py

---
+++ numpy_array_p44.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([[1, 0, 0],
+              [1, 1, 1],
+             [2, 0, 0]])
+
+print(np.max(a))
+#2
+
+print(np.min(a))
+#0
+
+print(np.average(a))
+#0.666666666
+
+
+
