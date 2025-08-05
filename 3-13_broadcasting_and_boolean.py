1f87c6e24d0b4f99f6c0f308d007313a713d36b8
Mike Smith <mlfxbroker@gmail.com>
2025-08-05 12:11:40 -0700

    Create 3-13_broadcasting_and_boolean.py

---
+++ 3-13_broadcasting_and_boolean.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+X = np.array([[1, 0, 0],
+              [0, 2, 2],
+              [3, 0, 0]])
+
+print(X == 2)
+"""
+== is the operator to see if X is 2. If it is or isn't will reply True of False
+"""
